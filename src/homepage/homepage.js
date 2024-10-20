fee8828282877e537fb5da318ac8c0e2eafc2873
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2024-10-20 19:02:13 +0530

    homepage, scoreboard design change.
    homepage, scoreboard design change.

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -30,19 +30,3 @@ document.querySelector('.btn').addEventListener('click', (event) => {
 });
 
 
-// Navigation Bar Code for Drop Down, Just copy it for other website.
-window.myFunction = function() {
-    document.getElementById("myDropdown").classList.toggle("show");
-};
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        const dropdowns = document.getElementsByClassName("dropdown-content");
-        for (let i = 0; i < dropdowns.length; i++) {
-            const openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-};
